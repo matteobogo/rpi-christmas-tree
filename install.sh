c5070d4c1055696c43c2298da1a7b7b84e02873c
root <matteo.bogo@gmail.com>
2018-12-12 04:12:26 +0100

    added crontab for setting start-stop times

--- install.sh
+++ install.sh
@@ -1,30 +1,22 @@
 #!/usr/bin/env bash
 
+_START_HOUR=17
+_START_MINUTE=0
+_END_HOUR=1
+_END_MINUTE=0
+
 _APP_NAME=xmas_tree.py
-_SERVICE_NAME=xmas_tree.service
 
 echo "installing dependencies.."
-sudo apt-get install python-gpiozero -y > /dev/null
-sudo apt-get install python3-gpiozero -y > /dev/null
+apt-get install python-gpiozero -y > /dev/null
+apt-get install python3-gpiozero -y > /dev/null
 
 echo "setting up the autostart.."
+
+crontab -l > xmas_tree
 echo "
-[Unit]
-Description=A program for handling the lights of a Christmas Tree
-After=multi-user.target
+    ${_START_MINUTE} ${_START_HOUR} * * * python ${PWD}/xmas_tree.py --mode=start &
+    ${_END_MINUTE} ${_END_HOUR} * * * python ${PWD}/xmas_tree.py -m stop && pkill -f xmas_tree" >> xmas_tree
 
-[Service]
-Type=idle
-ExecStart=/usr/bin/python $(pwd)/$_APP_NAME
-
-[Install]
-WantedBy=multi-user.target
-" > /lib/systemd/system/${_SERVICE_NAME}
-
-systemctl daemon-reload
-systemctl enable ${_SERVICE_NAME}
-
-echo "Rebooting.."
-sleep 5
-
-reboot
+crontab xmas_tree
+rm xmas_tree
--- xmas_tree.py
+++ xmas_tree.py
@@ -1,10 +1,44 @@
 from gpiozero import LEDBoard
 from gpiozero.tools import random_values
 from signal import pause
+import sys, getopt
 
-tree = LEDBoard(*range(2,28),pwm=True)
-for led in tree:
-    led.source_delay = 0.1
-    led.source = random_values()
+def main(argv):
 
-pause()
+    def start_blinking(tree):
+
+        print("start blinking..")
+        while True:
+            for led in tree:
+                led.source_delay = 0.1
+                led.source = random_values()
+
+    def stop_blinking(tree):
+
+        print("stop blinking..")
+        tree.off()
+
+    tree = LEDBoard(*range(2,28),pwm=True)
+
+    mode = None
+    err = "xmas_tree.py --mode <start | stop>"
+
+    try:
+        opts, args = getopt.getopt(argv,"m:",["mode="])
+    except getopt.GetoptError:
+        print(err)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-m", "--mode"):
+            mode = arg
+
+    if mode == 'stop':
+        stop_blinking(tree)
+    elif mode == 'start':
+        start_blinking(tree)
+    else:
+        print(err)
+        sys.exit(2)
+
+main(sys.argv[1:])
